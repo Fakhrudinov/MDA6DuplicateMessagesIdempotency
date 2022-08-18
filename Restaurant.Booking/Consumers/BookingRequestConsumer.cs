a95b4144c8de209261acd3f1a530a4a1df910b2e
Alexander Fakhrudinov <asbuka@gmail.com>
2022-08-18 21:19:58 +0300

    add init project

---
+++ Restaurant.Booking/Consumers/BookingRequestConsumer.cs
@@ -0,0 +1,44 @@
+﻿using MassTransit;
+using Restaurant.Messages.Interfaces;
+using System;
+using System.Threading.Tasks;
+using Restaurant.Booking.MassTransitDTO;
+using Restaurant.Messages.CustomExceptions;
+
+namespace Restaurant.Booking.Consumers
+{
+    public class BookingRequestConsumer : IConsumer<IBookingRequest>
+    {
+        private readonly Restaurant _restaurant;
+
+        public BookingRequestConsumer(Restaurant restaurant)
+        {
+            _restaurant = restaurant;
+        }
+
+        public async Task Consume(ConsumeContext<IBookingRequest> context)
+        {
+            Console.WriteLine($"BookingRequestConsumer==[OrderId: {context.Message.OrderId}] Ищем свободный стол");
+
+            // есть свободный стол?
+            var result = await _restaurant.BookFreeTableAsync(1, context.Message.OrderId, context.CancellationToken);
+
+            if (result == true)
+            {
+                Console.WriteLine($"BookingRequestConsumer==[OrderId: {context.Message.OrderId}] Столик найден");
+                await context.Publish<ITableBooked>(
+                    new TableBooked(
+                        context.Message.OrderId,
+                        context.Message.ClientId, 
+                        context.Message.Dish));
+            }
+            else
+            {
+                Console.WriteLine($"BookingRequestConsumer== [OrderId: {context.Message.OrderId}] Ошибка бронирования!");
+
+                //await context.Publish<IBookingReject>(new BookingReject(context.Message.OrderId));
+                throw new BookingException($"Ошибка бронирования! для заказа {context.Message.OrderId}");
+            }
+        }
+    }
+}
---
+++ Restaurant.Kitchen/Consumers/KitchenRequestedConsumer.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Threading.Tasks;
+using MassTransit;
+using Restaurant.Kitchen.MassTransitDTO;
+using Restaurant.Messages.CustomExceptions;
+using Restaurant.Messages.Interfaces;
+
+namespace Restaurant.Kitchen.Consumers
+{
+    internal class KitchenRequestedConsumer : IConsumer<ITableBooked>
+    {
+        private readonly Manager _manager;
+
+        public KitchenRequestedConsumer(Manager manager)
+        {
+            _manager = manager;
+        }
+
+        public async Task Consume(ConsumeContext<ITableBooked> context)
+        {
+            var randomDelay = new Random().Next(1_000, 10_000);
+            Console.WriteLine($"Kitchen-KitchenRequestedConsumer=Проверим заказ #{context.Message.OrderId} на кухне, [id={context.Message.Dish.Id}] это займет {randomDelay}мс");
+            await Task.Delay(randomDelay);
+
+            var (confirmation, dish) = _manager.CheckKitchenReady(context.Message.OrderId, context.Message.Dish);
+            
+            if (confirmation)
+            {
+                Console.WriteLine($"Kitchen-KitchenRequestedConsumer=заказ #{context.Message.OrderId} = ok, Publish KitchenReady");
+                await context.Publish<IKitchenReady>(new KitchenReady(context.Message.OrderId));
+            }
+            else
+            {
+                Console.WriteLine($"Kitchen-KitchenRequestedConsumer=заказ #{context.Message.OrderId} = failed, Publish KitchenAccident");
+                if (context.Message.Dish.Name != null)
+                {
+                    throw new KitchenException($"KitchenException - Заказ с {context.Message.Dish.Name} вызывает у нас проблемы. #{context.Message.OrderId}");
+                }
+                else
+                {
+                    throw new KitchenException($"KitchenException - Заказ #{context.Message.OrderId} - у нас нет такого в меню");
+                }                
+            }
+        }
+    }
+}
